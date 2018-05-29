89b533e1945bc26e7e9bcb6b9842dbfdf046ae25
Dylan Babbs <dylan.babbs@here.com>
2018-05-29 13:55:11 -0700

    add tooltips

---
+++ README.md
@@ -0,0 +1,21 @@
+# GeoSpace API // Deck.GL // Mapbox GL
+
+Using GeoSpace API to grab my flight history.
+
+Plotting with Uber's [Deck.GL](http://deck.gl) arc layer.
+
+Deck.GL is built on Mapbox GL.
+
+Custom HERE MVT tiles from CTO office.
+
+Ignore the `data` and `extra` directories.
+
+### To run:
+
+Download this repo, then start a web server in its directory:
+
+`python -m SimpleHTTPServer 8002`
+
+Then navigate to: [http://localhost:8002](http://localhost:8002)
+
+Make sure you are on the HERE network to access MVT tiles. If on VPN, ensure you are on the `HERE-Tunnel-ALL` group.
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var maphub = 'https://maphub.cit.api.here.com/geospace/x-test2/search';
+var maphub = 'https://maphub.cit.api.here.com/geospace/x-babbs-flights/search';
 var local = './data/trips.json';
 fetch(maphub, {
    headers: {
@@ -11,38 +11,133 @@ fetch(maphub, {
 })
    .then(res => res.json())
    .then(data => {
-      console.log(data);
-      var modified = [];
-      for (var i = 0; i < data.features.length; i++) {
-         modified.push(data.features[i].properties);
-      }
-
-      plot(modified);
+      var portsMod = createPorts(data);
+      var routesMod = data.features.map(x => {
+         return x.properties;
+      });
+      console.log(routesMod);
+      plot(routesMod, portsMod);
    });
 
-function plot(data) {
-   var layer = new deck.ArcLayer({
+function plot(data, portsMod) {
+   var arc = new deck.ArcLayer({
+      id: 'arc',
       data,
       // getSourcePosition: d => d.origin_coordinates,
-      // getTargetPosition: d => t.destination_coordinates,
+      // getTargetPosition: d => d.destination_coordinates,
       getSourceColor: d => [187, 2, 0, 150],
       getTargetColor: d => [59, 30, 177, 150],
-      strokeWidth: 5
+      //onHover: d => console.log('yoooooo'),
+      onHover: updateTooltip,
+      //onHover: ({ object }) => console.log(object.origin + '<>' + object.destination),
+      strokeWidth: 5,
+      pickable: true,
+      autoHighlight: true,
+      highlightColor: [50, 50, 50, 128]
    });
 
+   const scatter = new deck.ScatterplotLayer({
+      id: 'scatter',
+      data: portsMod,
+      pickable: true,
+      getPosition: d => d.coordinates,
+      getRadius: d => d.count * 20000,
+      getColor: d => [255, 255, 50],
+      //onHover: updateTooltip,
+      onHover: filter,
+      highlightColor: [50, 50, 50, 128], //currently not working?,
+      autoHighlight: true
+   });
+
+   function filter() {
+      console.log('adfasdfs')
+   }
+
    /* DeckGL */
-   new deck.DeckGL({
+   var deckGL = new deck.DeckGL({
       /* send it ✈️ */
+      map: mapboxgl,
       container: 'map',
-      mapStyle: 'style.json',
-      //MapboxAccessToken: 'pk.eyJ1IjoiZGJhYmJzIiwiYSI6ImNqN2d2aDBvczFkNmEycWt5OXo0YnY3ejkifQ.h1gKMs1F18_AhB09s91gWg',
-      longitude: -67.5,
+      mapboxApiAccessToken:
+         'pk.eyJ1IjoiZGJhYmJzIiwiYSI6ImNqN2d2aDBvczFkNmEycWt5OXo0YnY3ejkifQ.h1gKMs1F18_AhB09s91gWg',
+      mapStyle: 'mapbox://styles/mapbox/light-v9', //'https://free.tilehosting.com/styles/positron/style.json?key=U0iNgiZKlYdwvgs9UPm1',
+      longitude: -125.24414062499999,
       // layerFilter: true,
-      latitude:43.83452678223682,
-      zoom: 3,
+      latitude: 35.746512259918504,
+      zoom: 4,
       maxZoom: 88,
       pitch: 60,
       bearing: 50,
-      layers: [layer]
+      layers: [scatter, arc]
    });
+
+   scatter.updateState;
+
+   document.getElementById('send').onclick = function() {
+      console.log('hii');
+      console.log(deckGL);
+
+      const scatter2 = new deck.ScatterplotLayer({
+         id: 'scatter2',
+         data: portsMod,
+         pickable: true,
+         getPosition: d => d.coordinates,
+         getRadius: d => d.count * 100,
+         getColor: d => [10, 10, 50]
+      });
+
+      deckGL.setProps({
+         layers: [scatter2, arc]
+      });
+   };
+}
+
+function updateTooltip({ x, y, object }) {
+   const tooltip = document.getElementById('tooltip');
+   console.log(object)
+   if (object) {
+      tooltip.style.top = `${y}px`;
+      tooltip.style.left = `${x}px`;
+      if (object.date != null) { //arc info
+         tooltip.innerHTML = '<div>' + object.origin + '<>' + object.destination + '</div>';
+         tooltip.innerHTML += '<div>' + object.date + '</div>';
+         tooltip.innerHTML += '<div>' + object.airline + '</div>';
+      } else { //scatter info
+         tooltip.innerHTML = '<div>' + object.port + '</div>';
+         tooltip.innerHTML += '<div>' + object.count + ' arrivals</div>';
+      }
+
+   } else {
+      tooltip.innerHTML = '';
+   }
+}
+
+function createPorts(data) {
+   var ports = {};
+   for (var i = 0; i < data.features.length; i++) {
+      var origin = data.features[i].properties.origin;
+      var sourcePosition = data.features[i].properties.sourcePosition;
+      var destination = data.features[i].properties.destination;
+      var targetPosition = data.features[i].properties.targetPosition;
+      if (destination in ports) {
+         ports[destination] = {
+            count: ports[destination].count + 1,
+            position: targetPosition
+         };
+      } else {
+         ports[destination] = {
+            count: 1,
+            position: targetPosition
+         };
+      }
+   }
+   var portsMod = [];
+   Object.keys(ports).forEach(function(key) {
+      portsMod.push({
+         port: key,
+         count: ports[key].count,
+         coordinates: ports[key].position
+      });
+   });
+   return portsMod;
 }
