304586ff70f9b2409d26964aee47c7d1f603f670
Dylan Babbs <dylan.babbs@here.com>
2018-05-29 15:46:01 -0700

    hover functioning

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
+const deckgl = new deck.DeckGL({
+   /* send it ✈️ */
+   map: mapboxgl,
+   container: 'map',
+   mapboxApiAccessToken:
+      'pk.eyJ1IjoiZGJhYmJzIiwiYSI6ImNqN2d2aDBvczFkNmEycWt5OXo0YnY3ejkifQ.h1gKMs1F18_AhB09s91gWg',
+   mapStyle: 'mapbox://styles/mapbox/light-v9', //'https://free.tilehosting.com/styles/positron/style.json?key=U0iNgiZKlYdwvgs9UPm1',
+   longitude: -125.24414062499999,
+   // layerFilter: true,
+   latitude: 35.746512259918504,
+   zoom: 4,
+   maxZoom: 88,
+   pitch: 60,
+   bearing: 50
+});
+
+let data = null;
+let portsMod = null;
+let backup = null;
+
+
 var maphub = 'https://maphub.cit.api.here.com/geospace/x-babbs-flights/search';
-var local = './data/trips.json';
 fetch(maphub, {
    headers: {
       Accept: 'application/geo+json',
@@ -11,18 +31,24 @@ fetch(maphub, {
 })
    .then(res => res.json())
    .then(data => {
-      var portsMod = createPorts(data);
-      var routesMod = data.features.map(x => {
+      portsMod = createPorts(data);
+      data = data.features.map(x => {
          return x.properties;
       });
-      console.log(routesMod);
-      plot(routesMod, portsMod);
+      //backup = JSON.parse(JSON.stringify(data));
+      backup = data;
+      plot(data, portsMod);
+
+
    });
 
-function plot(data, portsMod) {
-   var arc = new deck.ArcLayer({
+function plot(t, portsMod) {
+
+   console.log("data being passed:")
+   console.log(t);
+   const arc = new deck.ArcLayer({
       id: 'arc',
-      data,
+      data: t,
       // getSourcePosition: d => d.origin_coordinates,
       // getTargetPosition: d => d.destination_coordinates,
       getSourceColor: d => [187, 2, 0, 150],
@@ -49,33 +75,41 @@ function plot(data, portsMod) {
       autoHighlight: true
    });
 
-   function filter() {
-      console.log('adfasdfs')
-   }
-
-   /* DeckGL */
-   var deckGL = new deck.DeckGL({
-      /* send it ✈️ */
-      map: mapboxgl,
-      container: 'map',
-      mapboxApiAccessToken:
-         'pk.eyJ1IjoiZGJhYmJzIiwiYSI6ImNqN2d2aDBvczFkNmEycWt5OXo0YnY3ejkifQ.h1gKMs1F18_AhB09s91gWg',
-      mapStyle: 'mapbox://styles/mapbox/light-v9', //'https://free.tilehosting.com/styles/positron/style.json?key=U0iNgiZKlYdwvgs9UPm1',
-      longitude: -125.24414062499999,
-      // layerFilter: true,
-      latitude: 35.746512259918504,
-      zoom: 4,
-      maxZoom: 88,
-      pitch: 60,
-      bearing: 50,
-      layers: [scatter, arc]
+   deckgl.setProps({
+    layers: [arc, scatter]
    });
 
-   scatter.updateState;
+   function filter({ layer, x, y, object }) {
+      const tooltip = document.getElementById('tooltip');
+
+      if (object) {
+         // console.log(deckgl.props);
+         // console.log(deckgl.props.layers);
+         // console.log(deckgl.props.layers[0]);
+         // console.log(deckgl.props.layers[0].props);
+         // console.log(deckgl.props.layers[0].props.data);
+         var temp = deckgl.props.layers[0].props.data;
+         let output = temp.filter((obj) => obj.origin == object.port);
+         plot(output, portsMod)
+
+         tooltip.style.top = `${y}px`;
+         tooltip.style.left = `${x}px`;
+         tooltip.innerHTML = '<div>' + object.port + '</div>';
+         tooltip.innerHTML += '<div>' + object.count + ' arrivals</div>';
+
+      } else {
+         console.log(deckgl.props.layers);
+         console.log("LEAVEEE")
+         plot(backup, portsMod);
+
+         tooltip.innerHTML = '';
+      }
+
+   }
 
    document.getElementById('send').onclick = function() {
       console.log('hii');
-      console.log(deckGL);
+      console.log(deckgl);
 
       const scatter2 = new deck.ScatterplotLayer({
          id: 'scatter2',
@@ -86,14 +120,14 @@ function plot(data, portsMod) {
          getColor: d => [10, 10, 50]
       });
 
-      deckGL.setProps({
+      deckgl.setProps({
          layers: [scatter2, arc]
       });
    };
 }
 
 function updateTooltip({ x, y, object }) {
-   const tooltip = document.getElementById('tooltip');
+
    console.log(object)
    if (object) {
       tooltip.style.top = `${y}px`;
@@ -114,11 +148,44 @@ function updateTooltip({ x, y, object }) {
 
 function createPorts(data) {
    var ports = {};
+
+   var group =[];
    for (var i = 0; i < data.features.length; i++) {
       var origin = data.features[i].properties.origin;
       var sourcePosition = data.features[i].properties.sourcePosition;
       var destination = data.features[i].properties.destination;
       var targetPosition = data.features[i].properties.targetPosition;
+
+
+      // var o = {name: origin, departures: 1, arrivals: 0, position: origin_position};
+      // var d = {name: destination, departures: 0, arrivals: 1, position: destination_position};
+      // if (i == 0) { //first
+      //    group.push(o);
+      //    group.push(d);
+      // } else {
+      //    var found = false;
+      //    for (var z = 0; z < group.length; z++) { //all
+      //       if (group[z][name] == o[name]) {
+      //          group[z][arrivals] = group[z][arrivals] + 1;
+      //          break;
+      //       }
+      //    }
+      // }
+
+
+
+      // if (origin in ports) {
+      //    ports[origin] = {
+      //       origin_count: ports[origin].origin_count + 1,
+      //       position: targetPosition
+      //    };
+      // } else {
+      //    ports[origin] = {
+      //       origin_count: 1,
+      //       position: targetPosition
+      //    };
+      // }
+
       if (destination in ports) {
          ports[destination] = {
             count: ports[destination].count + 1,
@@ -131,13 +198,18 @@ function createPorts(data) {
          };
       }
    }
+   console.log(ports);
+   //debugger;
    var portsMod = [];
    Object.keys(ports).forEach(function(key) {
       portsMod.push({
          port: key,
          count: ports[key].count,
+         //destination_count: ports[key].destination_count,
          coordinates: ports[key].position
       });
    });
+   console.log
+
    return portsMod;
 }
