bc781a9a8baa7ec7821d4eaf6705f9faa9aae479
Dylan Babbs <dylan.babbs@here.com>
2018-05-30 13:53:23 -0700

    temp

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const deckgl = new deck.DeckGL({
    mapboxApiAccessToken:
       'pk.eyJ1IjoiZGJhYmJzIiwiYSI6ImNqN2d2aDBvczFkNmEycWt5OXo0YnY3ejkifQ.h1gKMs1F18_AhB09s91gWg',
    mapStyle: 'mapbox://styles/dbabbs/cjhsk9yfx6sli2rtm1lbrze2j', //'mapbox://styles/mapbox/light-v9', //'https://free.tilehosting.com/styles/positron/style.json?key=U0iNgiZKlYdwvgs9UPm1',
+   // mapStyle: 'mapbox://styles/dbabbs/cjgfmampi000a2snu0f4touze',
    longitude: -57.38580902885856,
    // layerFilter: true,
    latitude: 62.51353296267838,
