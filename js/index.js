e0edf3104196b0090b5ff99f35eae11891b1f624
Dylan Babbs <dylan.babbs@gmail.com>
2018-10-01 11:41:11 -0700

    code cleanup

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@
 /* Setup & Interaction */
 /////////////////////////
 
-var $ = function(id) { return document.getElementById(id); };
+const $ = (id) => document.getElementById(id);
 
 //Enables panning & tilting via mouse interaction
 $('map').addEventListener('contextmenu', evt => evt.preventDefault());
@@ -39,26 +39,20 @@ let backup = null;
 let portBackup = null;
 
 //Slider
-var start = new Date('2/24/17');
-var end = new Date('6/9/18');
-var options = {
-    isDate: true,
-    min: start,
-    max: end,
-    start: start,
-    end: end,
-    overlap: true
+const start = new Date('2/24/17');
+const end = new Date('6/9/18');
+const options = {
+   isDate: true,
+   min: start,
+   max: end,
+   start: start,
+   end: end,
+   overlap: true
 };
-var slider = new Slider($('slider'), options);
+const slider = new Slider($('slider'), options);
 
-var xyz = 'https://xyz.api.here.com/hub/spaces/sPtFUG2Z/search';
-fetch(xyz, {
-   headers: {
-      Accept: 'application/geo+json',
-      'Authorization': 'Bearer IOc24KwI4ndNBxt922-myA'
-   }
-}).then(res => res.json()).then(data => {
-// fetch('data/trips.geojson').then(res => res.json()).then(data => {
+const xyz = 'https://xyz.api.here.com/hub/spaces/sPtFUG2Z/search?access_token=IOc24KwI4ndNBxt922-myA';
+fetch(xyz).then(res => res.json()).then(data => {
    data = data.features.map(x => x.properties);
 
    portsMod = createPorts(data);
@@ -69,14 +63,14 @@ fetch(xyz, {
    histogram(data);
 
    $('min').innerHTML = data[0].date.toLocaleString().split(",")[0];
-   $('max').innerHTML = data[data.length-1].date.toLocaleString().split(",")[0];
+   $('max').innerHTML = data[data.length - 1].date.toLocaleString().split(",")[0];
 
    slider.subscribe('moving', function(z) {
 
-      var temp = data.filter(obj => new Date(obj.date) >= new Date(z.left));
+      let temp = data.filter(obj => new Date(obj.date) >= new Date(z.left));
       temp = temp.filter(obj => new Date(obj.date) <= new Date(z.right));
 
-      var tempPorts = createPorts(temp)
+      const tempPorts = createPorts(temp)
 
       $('min').innerHTML = z.left.toLocaleString().split(",")[0];
       $('max').innerHTML = z.right.toLocaleString().split(",")[0];
@@ -92,14 +86,15 @@ fetch(xyz, {
 function plot(t, portsMod) {
 
    const arc = new deck.ArcLayer({
-      id: 'arc', data: t,
+      id: 'arc',
+      data: t,
       getSourceColor: d => [15, 129, 135],
       getTargetColor: d => [59, 30, 177],
       onHover: arcTooltip,
       strokeWidth: 5,
       pickable: true,
       autoHighlight: true,
-      highlightColor: [249, 205,23,250]
+      highlightColor: [249, 205, 23, 250]
    });
 
    const scatter = new deck.ScatterplotLayer({
@@ -130,11 +125,11 @@ function plot(t, portsMod) {
    deckgl.setProps({
       layers: [arc, scatter, scatterOutline],
       viewState: {
-        longitude: deckgl.getMapboxMap().getCenter().lng,
-        latitude: deckgl.getMapboxMap().getCenter().lat,
-        zoom: deckgl.getMapboxMap().getZoom(),
-        pitch: deckgl.getMapboxMap().getPitch(),
-        bearing: deckgl.getMapboxMap().getBearing()
+         longitude: deckgl.getMapboxMap().getCenter().lng,
+         latitude: deckgl.getMapboxMap().getCenter().lat,
+         zoom: deckgl.getMapboxMap().getZoom(),
+         pitch: deckgl.getMapboxMap().getPitch(),
+         bearing: deckgl.getMapboxMap().getBearing()
       },
    });
 }
@@ -146,8 +141,8 @@ function plot(t, portsMod) {
 //Scatterplot Filter
 function scatterFilter({layer, x, y, object}) {
    if (object) {
-      var temp = deckgl.props.layers[0].props.data;
-      var portTemp = deckgl.props.layers[1].props.data;
+      const temp = deckgl.props.layers[0].props.data;
+      const portTemp = deckgl.props.layers[1].props.data;
 
       let output = temp.filter(obj => obj.origin == object.port || obj.destination == object.port);
       let portOutput = portTemp.filter(obj => obj.port == object.port);
@@ -165,10 +160,10 @@ function scatterFilter({layer, x, y, object}) {
       //this is the only line of code that updates analytics outside of analytics:
       $("location").innerHTML = object.port;
    } else {
-      var newBackup = JSON.parse(JSON.stringify(backup))
-      var temp = newBackup.filter(obj => new Date(obj.date) >= new Date(slider.getInfo().left));
+      const newBackup = JSON.parse(JSON.stringify(backup))
+      let temp = newBackup.filter(obj => new Date(obj.date) >= new Date(slider.getInfo().left));
       temp = temp.filter(obj => new Date(obj.date) <= new Date(slider.getInfo().right));
-      var newPortBackup = createPorts(temp);
+      const newPortBackup = createPorts(temp);
       plot(temp, newPortBackup);
       analytics(temp)
 
@@ -203,13 +198,13 @@ function arcTooltip({x, y, object}) {
 
 //Creates the dataset for the scatterplot
 function createPorts(data) {
-   var ports = {};
-   var group = [];
-   for (var i = 0; i < data.length; i++) {
-      var origin = data[i].origin;
-      var sourcePosition = data[i].sourcePosition;
-      var destination = data[i].destination;
-      var targetPosition = data[i].targetPosition;
+   const ports = {};
+   const group = [];
+   for (let i = 0; i < data.length; i++) {
+      const origin = data[i].origin;
+      const sourcePosition = data[i].sourcePosition;
+      const destination = data[i].destination;
+      const targetPosition = data[i].targetPosition;
 
       if (destination in ports) {
          ports[destination] = {
@@ -223,10 +218,11 @@ function createPorts(data) {
          };
       }
    }
-   var portsMod = [];
+   const portsMod = [];
    Object.keys(ports).forEach(function(key) {
       portsMod.push({
-         port: key, count: ports[key].count,
+         port: key,
+         count: ports[key].count,
          coordinates: ports[key].position
       });
    });
@@ -235,12 +231,12 @@ function createPorts(data) {
 
 //Prepare data for sidebar statistics
 function analytics(data) {
-   var distance = 0;
-   var airlines = {}
-   var airport = data[0].origin;
-   for (var i = 0; i < data.length; i++) {
+   let distance = 0;
+   const airlines = {}
+   const airport = data[0].origin;
+   for (let i = 0; i < data.length; i++) {
       distance += data[i].distance;
-      var airline = data[i].airline;
+      const airline = data[i].airline;
 
       if (airline in airlines) {
          airlines[airline] = airlines[airline] + 1;
@@ -258,7 +254,7 @@ function analytics(data) {
 /////////////
 
 //Changing viewport / "Flying"
-var views = {
+const views = {
    defaultView: {
       name: 'World',
       longitude: -57.38580902885856,
@@ -286,8 +282,8 @@ var views = {
 }
 
 //Creating flying dropdown
-for (var i = 0; i < Object.keys(views).length; i++) {
-   var option = document.createElement('option');
+for (let i = 0; i < Object.keys(views).length; i++) {
+   const option = document.createElement('option');
    option.innerText = Object.values(views)[i]['name'];
    option.id = Object.keys(views)[i];
    $('flys').appendChild(option);
@@ -296,32 +292,34 @@ $('flys').onchange = fly;
 
 //Perform fly
 function fly() {
-   var x = $('flys').options[$('flys').selectedIndex].id;
+   const x = $('flys').options[$('flys').selectedIndex].id;
    deckgl.setProps({
       viewState: {
-       longitude: views[x].longitude,
-       latitude: views[x].latitude,
-       zoom: views[x].zoom,
-       pitch: views[x].pitch,
-       bearing: views[x].bearing
+         longitude: views[x].longitude,
+         latitude: views[x].latitude,
+         zoom: views[x].zoom,
+         pitch: views[x].pitch,
+         bearing: views[x].bearing
       },
-    transitionInterpolator: new deck.experimental.ViewportFlyToInterpolator(),
-    transitionDuration: 2500
-  })
+      transitionInterpolator: new deck.experimental.ViewportFlyToInterpolator(),
+      transitionDuration: 2500
+   })
 }
 
 ////////////
 /* Themes */
 ////////////
-var themes = ["dark", "light"];
-for (var i = 0; i < themes.length; i++) {
-   var option = document.createElement('option');
+const themes = ["dark", "light"];
+for (let i = 0; i < themes.length; i++) {
+   const option = document.createElement('option');
    option.innerText = themes[i].charAt(0).toUpperCase() + themes[i].slice(1) + ' Theme';
    option.id = themes[i];
    $('themes').appendChild(option);
 }
 
-$('themes').onchange = () => { $('themes').options[$('themes').selectedIndex].id == 'light' ? light() : dark() };
+$('themes').onchange = () => {
+   $('themes').options[$('themes').selectedIndex].id == 'light' ? light() : dark()
+};
 
 
 function light() {
@@ -331,13 +329,13 @@ function light() {
    $("info").style.backgroundColor = '#F1F1F2';
    document.querySelector("h2").style.color = "#333";
 
-   var captions = document.querySelectorAll(".caption");
-   for (var i = 0; i < captions.length; i++) {
+   const captions = document.querySelectorAll(".caption");
+   for (let i = 0; i < captions.length; i++) {
       captions[i].style.color = "#6F6F6F";
    }
 
-   var stats = document.querySelectorAll(".stat");
-   for (var i = 0; i < stats.length; i++) {
+   const stats = document.querySelectorAll(".stat");
+   for (let i = 0; i < stats.length; i++) {
       stats[i].style.color = "#333";
    }
 
@@ -351,26 +349,26 @@ function light() {
    $("themes").style.borderColor = '#D4D4D4';
    $("themes").style.color = '#6F6F6F';
 
-   var bars = document.querySelectorAll("rect.bar");
-   for (var i = 0; i < bars.length; i++) {
+   const bars = document.querySelectorAll("rect.bar");
+   for (let i = 0; i < bars.length; i++) {
       bars[i].style.fill = '#919193';
    }
 }
 
 function dark() {
    deckgl.getMapboxMap().setStyle('styles/xyz-osm-dark.json');
-   $("map").style.backgroundColor = '#0e1d2a'
+   $("map").style.backgroundColor = 'rgba(19, 37, 52, 1)'
 
    $("info").style.backgroundColor = '#1D1E27';
    document.querySelector("h2").style.color = "#fff";
 
-   var captions = document.querySelectorAll(".caption");
-   for (var i = 0; i < captions.length; i++) {
+   const captions = document.querySelectorAll(".caption");
+   for (let i = 0; i < captions.length; i++) {
       captions[i].style.color = "#919193";
    }
 
-   var stats = document.querySelectorAll(".stat");
-   for (var i = 0; i < stats.length; i++) {
+   const stats = document.querySelectorAll(".stat");
+   for (let i = 0; i < stats.length; i++) {
       stats[i].style.color = "#fff  ";
    }
 
@@ -384,42 +382,42 @@ function dark() {
    $("themes").style.borderColor = '#919191';
    $("themes").style.color = '#fff';
 
-   var bars = document.querySelectorAll("rect.bar");
-   for (var i = 0; i < bars.length; i++) {
+   const bars = document.querySelectorAll("rect.bar");
+   for (let i = 0; i < bars.length; i++) {
       bars[i].style.fill = "#4B5157";
    }
 }
 
 function histogram(data) {
 
-   var margin = {
-         top: 0,
-         right: 0,
-         bottom: 5,
-         left: 0
-      }
+   const margin = {
+      top: 0,
+      right: 0,
+      bottom: 5,
+      left: 0
+   }
 
-   var width = width = $("graph").offsetWidth;
-   var height = 40 - margin.top - margin.bottom;
+   const width = $("graph").offsetWidth;
+   const height = 40 - margin.top - margin.bottom;
 
-   var parseDate = d3.timeParse("%m/%d/%y");
-   var x = d3.scaleTime().domain([
+   const parseDate = d3.timeParse("%m/%d/%y");
+   const x = d3.scaleTime().domain([
       new Date(2017, 1, 20),
       new Date(2018, 5, 10)
    ]).rangeRound([0, width]);
-   var y = d3.scaleLinear().range([height, 0]);
+   const y = d3.scaleLinear().range([height, 0]);
 
-   var histogram = d3.histogram().value(function(d) {
+   const histogram = d3.histogram().value(function(d) {
       return d.date;
    }).domain(x.domain()).thresholds(x.ticks(d3.timeWeek));
 
-   var svg = d3.select("#graph").append("svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+   const svg = d3.select("#graph").append("svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
    data.forEach(function(d) {
       d.date = parseDate(d.date);
    });
 
-   var bins = histogram(data);
+   const bins = histogram(data);
 
    y.domain([
       0,
@@ -431,7 +429,7 @@ function histogram(data) {
    svg.selectAll("rect").data(bins).enter().append("rect").attr("class", "bar").attr("x", 1).attr("transform", function(d) {
       return "translate(" + x(d.x0) + "," + y(d.length) + ")";
    }).attr("width", function(d) {
-      return x(d.x1) - x(d.x0) - 1;
+      return x(d.x1) - x(d.x0) - 1 > 0 ? x(d.x1) - x(d.x0) - 1 : 0;
    }).attr("height", function(d) {
       return height - y(d.length);
    });
